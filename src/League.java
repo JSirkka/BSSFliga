5861b0480ecf9933cee01b44587a4514e80dcb9d
jonas <jsirkka98@gmail.com>
2025-05-19 17:02:25 +0200

    Test

---
+++ src/League.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+
+public class League {
+
+    private ArrayList<Team> leagueList;
+}
---
+++ src/Team.java
@@ -0,0 +1,9 @@
+public class Team {
+    private String name;
+
+
+    public String getName(){
+        return name;
+    }
+
+}
